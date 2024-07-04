34fcdfb906076e76bfbfe1ba96b53683d336744a
dibsnvas <d_beisenbayeva@kbtu.kz>
2024-07-04 16:41:37 +0500

    initial commit

--- back/main.py
+++ back/main.py
@@ -1,33 +1,24 @@
-# uvicorn main:app
-# uvicorn main:app --reload
-
-
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
 import openai
 
-
 from functions.text_to_speech import convert_text_to_speech
 from functions.openai_requests import convert_audio_to_text, get_chat_response
 from functions.database import store_messages, reset_messages
 
-
 openai.organization = config("OPEN_AI_ORG")
 openai.api_key = config("OPEN_AI_KEY")
 
-
 app = FastAPI()
-
 
 origins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:4173",
     "http://localhost:3000",
-    ]
-
+]
 
 app.add_middleware(
     CORSMiddleware,
@@ -37,21 +28,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the FastAPI application"}
 
 @app.get("/health")
 async def check_health():
     return {"response": "healthy"}
-
 
 @app.get("/reset")
 async def reset_conversation():
     reset_messages()
     return {"response": "conversation reset"}
 
-
 @app.post("/post-audio/")
 async def post_audio(file: UploadFile = File(...)):
-
     with open(file.filename, "wb") as buffer:
         buffer.write(file.file.read())
     audio_input = open(file.filename, "rb")
